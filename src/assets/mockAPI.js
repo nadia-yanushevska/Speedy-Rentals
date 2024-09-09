210b077a466a70cbb490f3619cbcd16b96009c46
Nadia <67208287+nadia-yanushevska@users.noreply.github.com>
2024-09-09 23:52:02 +0300

    Update mockAPI.js

--- src/assets/mockAPI.js
+++ src/assets/mockAPI.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const mockApiID = '66d8a06b37b1cadd805542d9'
-axios.defaults.baseURL = 'https://${mockApiID}.mockapi.io';
+axios.defaults.baseURL = `https://${mockApiID}.mockapi.io`;
 
 export function get_cars(page = 1) {
     const data = axios.get(`/adverts/${page}`);
